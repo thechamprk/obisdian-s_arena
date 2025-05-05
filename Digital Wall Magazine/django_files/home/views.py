3a40699708775433535f03c30dcb906cf016dda7
thechamprk <rounak.k.mahto@gmail.com>
2025-05-05 13:49:42 +0530

    updating home page

--- Digital Wall Magazine/django_files/home/views.py
+++ Digital Wall Magazine/django_files/home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This is Homepage")
+
+def blogs(request):
+    return HttpResponse("This is Blogs")
+
+def art_gallery(request):
+    return HttpResponse("This is Art Gallery")
+
+def notices(request):
+    return HttpResponse("This is Notices")
+
+def about_us(request):
+    return HttpResponse("This is About Us")
